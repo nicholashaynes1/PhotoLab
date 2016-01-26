52eadd96dd75172b23f04ac3c8d055783448ed3f
nicholashaynes1 <nicholashaynes1@gmail.com>
2016-01-26 12:29:51 -0700

    Added methods!

--- src/pixLab/classes/Picture.java
+++ src/pixLab/classes/Picture.java
@@ -210,8 +210,8 @@ public class Picture extends SimplePicture
   /** Method to create a collage of several pictures */
   public void createCollage()
   {
-    Picture flower1 = new Picture("flower1.jpg");
-    Picture flower2 = new Picture("flower2.jpg");
+    Picture flower1 = new Picture("robot.jpg");
+    Picture flower2 = new Picture("swan.jpg");
     this.copy(flower1,0,0);
     this.copy(flower2,100,0);
     this.copy(flower1,200,0);
@@ -262,7 +262,11 @@ public class Picture extends SimplePicture
     beach.explore();
     beach.zeroBlue();
     beach.explore();
-    beach.zeroRed();
+    beach.createCollage();
+    beach.explore();
+    beach.mirrorVertical();
+    beach.explore();
+    beach.mirrorTemple();
     beach.explore();
     beach.zeroGreen();
     beach.explore();
